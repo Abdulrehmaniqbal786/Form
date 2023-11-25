188fc87127d056ef3260c42d0ff19bbb5d47300c
Abdulrehmaniqbal786 <147655636+Abdulrehmaniqbal786@users.noreply.github.com>
2023-11-25 11:59:41 -0800

    complte

---
+++ app.js
@@ -0,0 +1,7 @@
+// var div = document.getElementById("main")
+// var d = document.createElement("a")
+
+// var g = document.createTextNode("google");
+// d.appendChild(g)
+// d.setAttribute("href","https://google.com")
+// div.appendChild(d)
